fb1fd20abcd4e36de266edb4186a8f205d17a750
Nagachaithanya <nagachaithanya@latlong.ai>
2025-09-29 09:04:12 +0530

    coomited from nc lap

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,39 +1,44 @@
-"use client";
+ "use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "../ui/button";
-import Logo from "@/public/assests/logo.png";
-export default function Navbar() {
+import Logo  from "../../public/assets/Tanpura.png"
+import { Noto_Sans } from "next/font/google";
+
+const Noto = Noto_Sans({
+  subsets: ["devanagari"],
+  variable: "--font-noto",
+});
+
+export default function navbar() {
+  const WHATSAPP_PHONE_NUMBER = "9606009998";
+
   return (
-    <div className="flex items-center justify-between bg-gradient-to-r from-[#355C7D] via-[#6C5B7B] to-[#C06C84] h-20 px-8 font-medium text-lg text-white shadow-md">
-      <Link href="/" className="flex items-center gap-2">
-        <Image src={Logo} alt="logo" width={60} height={60} />
+    <div
+      id="nav-bg"
+      className="flex items-center justify-between bg-[#1a121b] h-20 px-8 font-medium text-lg text-[#f5f5dc] shadow-md"
+    >
+      <Link href="/" className="flex items-center gap-x-4">
+        <Image src={Logo} alt="logo" width={80} height={60} />
+        <span
+          className={`-ml-8 font-semibold text-2xl tracking-wide text-[#d87078] hidden sm:block ${Noto.variable}`}
+        >
+          स्वर माया
+        </span>
       </Link>
-
-      <ul className="flex gap-6 font-sans">
-        <li>
-          <Link href="/episodes">Episodes</Link>
-        </li>
-        <li>
-          <Link href="/blogs">Blogs</Link>
-        </li>
-        <li>
-          <div className="h-6 w-0.5 bg-black mt-1"></div>{" "}
-        </li>
-
-        <li>
-          <Link href="/about">About Us</Link>
-        </li>
-        <li>
-          <Link href="/subscribe">Subscribe</Link>
-        </li>
-        <li>
-          <Link href="/reviews">Reviews</Link>
-        </li>
-      </ul>
-
-      <Button className="cursor-pointer rounded-2xl w-28 p-2">Register</Button>
+      <div>
+        <a
+          href={`https://wa.me/${WHATSAPP_PHONE_NUMBER}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative group overflow-hidden rounded-full transition-transform duration-300 transform hover:scale-105 shadow-lg"
+        >
+          <Button className="bg-[#d87078] text-[#1a121b] cursor-pointer rounded-full h-10 px-6 font-bold text-base transition-colors duration-300 hover:bg-[#c5a45e] hover:text-[#1a121b]">
+            Contact Us
+          </Button>
+        </a>
+      </div>
     </div>
   );
 }
